9cc9548f65e9152ff7160586edefb6a666ebe649
Learner062022 <ddylan246@yahoo.com>
2025-04-29 19:26:50 +0800

    feat(main-page): enhance layout handling and integrate ViewModel
    - Added dynamic grid layout configuration to optimize UI for portrait and landscape orientations.
    - Integrated MainPageViewModel for improved separation of concerns using MVVM pattern.
    - Refactored button click event handler for better handling of numeric and functional button inputs.
    - Improved maintainability and readability through modular design.
    
    BREAKING CHANGE: Adjusted layout logic in MainPageSizeChanged, which may require updates in related components.

--- DylanDeSouzaTallyApp/DylanDeSouzaTallyApp/MainPage.xaml.cs
+++ DylanDeSouzaTallyApp/DylanDeSouzaTallyApp/MainPage.xaml.cs
@@ -1,48 +1,67 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace DylanDeSouzaTallyApp
 {
     public partial class MainPage : ContentPage
     {
+        readonly MainPageViewModel _viewModel = new MainPageViewModel();
+
         public MainPage()
         {
             InitializeComponent();
-            SizeChanged += MainPageSizeChanged;
+            BindingContext = _viewModel;
+            SizeChanged += OnMainPageSizeChanged;
         }
 
-        void MainPageSizeChanged(object sender, EventArgs e)
+        void OnMainPageSizeChanged(object sender, EventArgs e)
         {
             bool isPortrait = Height > Width;
 
-            if (isPortrait)
-            {
-                firstColumn.Width = new GridLength(1, GridUnitType.Star);
-                secondColumn.Width = new GridLength(0);
-            }
-            else
-            {
-                firstColumn.Width = new GridLength(1, GridUnitType.Star);
-                secondColumn.Width = new GridLength(1, GridUnitType.Star);
-            }
+            if (isPortrait) ConfigureGridLayout(editorGrid, keypad, 0, 0, 1, 2, 1, 0, 1, 2);
+            else ConfigureGridLayout(editorGrid, keypad, 0, 0, 2, 1, 0, 1, 2, 1);
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        void ConfigureGridLayout(
+            Grid editorGrid, Grid keypad,
+            int editorRow, int editorColumn, int editorRowSpan, int editorColumnSpan,
+            int keypadRow, int keypadColumn, int keypadRowSpan, int keypadColumnSpan)
         {
-            Button button = (Button)sender;
-            TalliedNumbers.CreateNum(button);
-            TalliedNumbers.AddNum(button);
-            if (button.Text == "+")
+            Grid.SetRow(editorGrid, editorRow);
+            Grid.SetColumn(editorGrid, editorColumn);
+            Grid.SetRowSpan(editorGrid, editorRowSpan);
+            Grid.SetColumnSpan(editorGrid, editorColumnSpan);
+
+            Grid.SetRow(keypad, keypadRow);
+            Grid.SetColumn(keypad, keypadColumn);
+            Grid.SetRowSpan(keypad, keypadRowSpan);
+            Grid.SetColumnSpan(keypad, keypadColumnSpan);
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
             {
-                string item = TalliedNumbers.GetLastNum();
-                TalliedNumbers.DspLastNum(talliedNums, item);
-                TalliedNumbers.SumNums(item);
-                TalliedNumbers.DspSum(totalAmount);
-            }
-            if (button.Text == "C")
-            {
-                TalliedNumbers.Reset(talliedNums);
+                if (int.TryParse(button.Text, out _))
+                {
+                    _viewModel.UpdateNumberEntered(button.Text);
+                }
+                else
+                {
+                    switch (button.Text)
+                    {
+                        case "+":
+                            _viewModel.AddNumber();
+                            break;
+                        case "C":
+                            _viewModel.Clear();
+                            break;
+                        default:
+                            Debug.WriteLine($"Unexpected button text: {button.Text}");
+                            break;
+                    }
+                }
             }
         }
     }
